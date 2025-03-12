94ddbbc313e23e3bde4b837eab8e319223deca08
Eron Castro <eroncastro@gmail.com>
2025-03-12 17:01:45 -0300

    Fix formatting

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,33 @@
 use std::{cell::RefCell, rc::Rc};
 
-
 #[derive(Clone)]
 pub struct Node<T: Clone + PartialEq> {
-	data: T,
-	next: Option<Rc<RefCell<Node<T>>>>,
+    data: T,
+    next: Option<Rc<RefCell<Node<T>>>>,
 }
 
 pub struct LinkedList<T: Clone + PartialEq> {
-	head: Option<Rc<RefCell<Node<T>>>>,
+    head: Option<Rc<RefCell<Node<T>>>>,
     tail: Option<Rc<RefCell<Node<T>>>>,
 }
 
 impl<T: Clone + PartialEq> LinkedList<T> {
-	pub fn new() -> Self {
-		LinkedList {
+    pub fn new() -> Self {
+        LinkedList {
             head: None,
             tail: None,
         }
-	}
+    }
 
     pub fn is_empty(&self) -> bool {
         self.head.is_none() || self.tail.is_none()
     }
 
-	pub fn push(&mut self, data: T) {
-		let nn = Rc::new(RefCell::new(Node {
-			data: data,
-			next: None,
-		}));
+    pub fn push(&mut self, data: T) {
+        let nn = Rc::new(RefCell::new(Node {
+            data: data,
+            next: None,
+        }));
 
         if self.head.is_none() {
             self.head = Some(Rc::clone(&nn));
@@ -40,33 +39,33 @@ impl<T: Clone + PartialEq> LinkedList<T> {
         }
 
         self.tail = Some(Rc::clone(&nn));
-	}
+    }
 
-	pub fn collect(&self) -> Option<Vec<T>> {
+    pub fn collect(&self) -> Option<Vec<T>> {
         if self.is_empty() {
             return None;
         }
 
-		let mut out = Vec::<T>::new();
+        let mut out = Vec::<T>::new();
 
-		let mut cur = self.head.clone();
-		while let Some(n) = cur {
+        let mut cur = self.head.clone();
+        while let Some(n) = cur {
             let item = n.borrow();
-			out.push(item.data.clone());
-			cur = item.clone().next;
-		}
+            out.push(item.data.clone());
+            cur = item.clone().next;
+        }
 
         Some(out)
-	}
+    }
 
     pub fn remove(&mut self, data: T) {
         if self.is_empty() {
-            return
+            return;
         }
 
         let mut prev: Option<Rc<RefCell<Node<T>>>> = None;
         let mut cur: Option<Rc<RefCell<Node<T>>>> = self.head.clone();
-		while let Some(n) = cur {
+        while let Some(n) = cur {
             let item = n.borrow();
             if item.data != data {
                 prev = Some(n.clone());
@@ -80,8 +79,8 @@ impl<T: Clone + PartialEq> LinkedList<T> {
                     self.head = None;
                     self.tail = None;
                 } else {
-                   self.head = item.next.clone();
-                   self.tail = item.next.clone();
+                    self.head = item.next.clone();
+                    self.tail = item.next.clone();
                 }
             } else {
                 if item.next.is_none() {
@@ -92,26 +91,25 @@ impl<T: Clone + PartialEq> LinkedList<T> {
                 }
             }
 
-            break
-		}
-
+            break;
+        }
     }
 }
 
 #[cfg(test)]
 #[test]
 fn test_linked_list_push_collect() {
-	let mut list = LinkedList::<i32>::new();
-	list.push(1);
-	list.push(2);
+    let mut list = LinkedList::<i32>::new();
+    list.push(1);
     list.push(2);
-	list.push(3);
-	list.push(4);
+    list.push(2);
+    list.push(3);
+    list.push(4);
     list.remove(2);
     list.remove(4);
     list.remove(5);
     list.push(5);
-	assert_eq!(list.collect().unwrap(), vec![1, 2, 3, 5]);
+    assert_eq!(list.collect().unwrap(), vec![1, 2, 3, 5]);
 }
 
 fn main() {
